94bb401ee5e0d9e84a03714fa380dfff120a750e
Tarik Onalan <phi.quantum@gmail.com>
2014-08-22 07:16:41 -0700

    Initial commit

---
+++ api.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    request = require('request'),
+    cheerio = require('cheerio')
+
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+var port = process.env.PORT || 8080
+
+var router = express.Router()
+
+router.get('/', function(req, res) {
+    res.json({ "message": "Not much to see here." })
+})
+
+var whatif = "http://whatif.xkcd.com/",
+    blog = "http://blog.xkcd.com"
+
+app.use('/api', router)
+
+app.listen(port)
+console.log('API is located on port ' + port)
