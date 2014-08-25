904a1ff2ab0da0eba05c0e7faa1d2a3f586e2a54
Tarik Onalan <phi.quantum@gmail.com>
2014-08-25 11:49:31 -0700

    Fixed api

--- index.js
+++ index.js
@@ -16,17 +16,28 @@ router.get('/', function(req, res) {
     res.json({ "message": "No velociraptors." })
 })
 
-router.get('/xkcd', function(req, res) {
-    request(xkcd + '/info.0.json', function(err, resp, body) {
-        if(err) {
-            res.json({ "message": err.message })
-            return
-        }
-        var info = JSON.parse(body)
-        var json = { "num": info["num"] }
-        res.json(json)
+router.route('/xkcd')
+    .get(function(req, res) {
+        request(xkcd + '/info.0.json', function(err, resp, body) {
+            if(err) {
+                res.json(resp.statusCode, { "message": err.message })
+                return
+            }
+            res.json(JSON.parse(body.replace(/\\n/g, ' ')))
+        })
     })
-})
+
+    .post(function(req, res) {
+        request(xkcd + '/info.0.json', function(err, resp, body) {
+            if(err) {
+                res.json(resp.statusCode, { "message": err.message })
+                return
+            }
+            var info = JSON.parse(body)
+            var json = { "num": info["num"] }
+            res.json(json)
+        })
+    })
 
 router.get('/xkcd/:id', function(req, res) {
     request(xkcd + '/' + req.params.id + '/info.0.json', function(err, resp, body) {
@@ -38,17 +49,72 @@ router.get('/xkcd/:id', function(req, res) {
     })
 })
 
-router.get('/whatif', function(req, res) {
-    request(whatif, function(err, resp, body) {
-        if(err) {
-            res.json(resp.statusCode, { "message": err.message })
-            return
-        }
-        $ = cheerio.load(body)
-        var num = parseInt($('article.entry > a').attr('href').split('/')[3])
-        res.json({ "num": num })
+router.route('/whatif')
+    .post(function(req, res) {
+        request(whatif, function(err, resp, body) {
+            if(err) {
+                res.json(resp.statusCode, { "message": err.message })
+                return
+            }
+            $ = cheerio.load(body)
+            var num = parseInt($('article.entry > a').attr('href').split('/')[3])
+            res.json({ "num": num })
+        })
     })
-})
+
+    .get(function(req, res) {
+        request(whatif, function(err, resp, body) {
+            if(err) {
+                res.json(resp.statusCode, { "message": err.message })
+                return
+            }
+            $ = cheerio.load(body)
+            var entry = $('article.entry'),
+                title = $('article.entry > a > h1').text()
+                question = $('p#question'),
+                attribute = $('p#attribute'),
+                content = $('article.entry > p'),
+                images = $('img.illustration'),
+                layout = [],
+                contemp = [],
+                imgtemp = []
+
+            content.each(function(index, element) {
+                element = $(element)
+                if(element.attr('id') !== "question" &&
+                        element.attr('id') !=="attribute")
+                    contemp.push(element.text())
+            })
+            content = contemp
+
+            images.each(function(index, element) {
+                imgtemp.push(whatif + $(element).attr('src'))
+            })
+            images = imgtemp
+
+            entry.children().each(function(index, element) {
+                element = $(element)
+                if(element.is('p'))
+                    layout.push('p')
+                else if(element.is('img'))
+                    layout.push('img')
+            })
+
+            question = question.text()
+            attribute = attribute.text()
+
+            res.json(JSON.parse(JSON.stringify(
+                {
+                    "title": title,
+                    "question": question,
+                    "attribute": attribute,
+                    "content": content.join('|'),
+                    "img": images.join('|'),
+                    "layout": layout.join('|')
+                }
+            ).replace(/\\n/g, ' ')))
+        })
+    })
 
 router.get('/whatif/:id', function(req, res) {
     request(whatif + '/' + req.params.id, function(err, resp, body) {
@@ -96,9 +162,9 @@ router.get('/whatif/:id', function(req, res) {
                 "title": title,
                 "question": question,
                 "attribute": attribute,
-                "content": content.join('_'),
-                "img": images.join('_'),
-                "layout": layout.join('_')
+                "content": content.join('|'),
+                "img": images.join('|'),
+                "layout": layout.join('|')
             }
         ).replace(/\\n/g, ' ')))
     })
